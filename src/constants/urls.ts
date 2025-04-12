4244dfa91c3344237e60fbf40a4c3e86052916c2
Fiser <ruben@nexolabs.xyz>
2025-04-12 14:57:55 +0200

    feat: added icon links to the social networks

---
+++ src/constants/urls.ts
@@ -0,0 +1,6 @@
+export const YOUTUBE_URL = "https://www.youtube.com/@f-techenglish295";
+export const INSTAGRAM_URL = "#"; 
+export const FACEBOOK_URL = "#";
+export const X_URL = "#";
+export const EMAIL_URL = "mailto:mogami.creative@gmail.com";
+export const BOOK_LESSON_URL = "https://calendly.com/nexolabs/consultoria-uno-a-uno";
