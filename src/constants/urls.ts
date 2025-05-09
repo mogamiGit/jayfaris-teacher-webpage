5ae61123498af43a5c19d9a60097e148c8869ee2
Fiser <ruben@nexolabs.xyz>
2025-05-09 19:30:28 +0200

    feat: added content changes

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -2,5 +2,5 @@ export const YOUTUBE_URL = "https://www.youtube.com/@f-techenglish295";
 export const INSTAGRAM_URL = "#";
 export const FACEBOOK_URL = "#";
 export const X_URL = "#";
-export const EMAIL_URL = "mailto:#";
-export const BOOK_LESSON_URL = "https://calendly.com/nexolabs/consultoria-uno-a-uno";
+export const EMAIL_URL = "mailto:#jayfaris72@gmail.com ";
+export const BOOK_LESSON_URL = "https://jayfaris.youcanbook.me";
