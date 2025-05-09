6e439aed39d7851f283f1514962a98bcc3aa183a
Mónica Galán <mogami.creative@gmail.com>
2025-05-09 16:17:30 +0200

    feat: Rive framework added, HoobieBullets new animations added

--- src/components/HobbieBullet.tsx
+++ src/components/HobbieBullet.tsx
@@ -1,18 +1,31 @@
 import React from 'react';
 import * as motion from "motion/react-client";
+import { useRive } from '@rive-app/react-canvas';
 
 interface Props {
   title: string;
   description: string;
   urlImage: string;
   altImage: string;
+  withRive?: boolean;
 }
 
-const HobbieBullet: React.FC<Props> = ({ title, description, ...rest }) => {
+const HobbieBullet: React.FC<Props> = ({ title, description, withRive = false, ...rest }) => {
+  const { RiveComponent } = useRive({
+    src: rest.urlImage,
+    autoplay: true,
+    stateMachines: "State Machine 1",
+  });
 
   return (
     <div className="max-w-[18rem] flex flex-col gap-2 items-center sm:items-start">
-      <img src={rest.urlImage} alt={rest.altImage} className='max-w-[160px]' />
+      <div className="w-40 h-40">
+        {withRive ? (
+          <RiveComponent />
+        ) : (
+          <img src={rest.urlImage} alt={rest.altImage} className='w-40' />
+        )}
+      </div>
       <p className="font-bold text-xl">{title}</p>
       <p className='leading-[1.6] text-sm text-center sm:text-left'>{description}</p>
     </div>
