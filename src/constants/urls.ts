d3d82863aa562523d01c37910e8f142086a7a313
Fiser <ruben@nexolabs.xyz>
2025-05-13 15:29:09 +0200

    feat: added more urls

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,8 +1,8 @@
 export const YOUTUBE_URL = "https://www.youtube.com/@f-techenglish295";
-export const INSTAGRAM_URL = "#";
-export const FACEBOOK_URL = "#";
-export const X_URL = "#";
-export const EMAIL_URL = "mailto:#jayfaris72@gmail.com ";
+export const INSTAGRAM_URL = "https://www.instagram.com/shinkoiwanojay72/";
+export const FACEBOOK_URL = "https://www.facebook.com/jay.faris.9";
+export const X_URL = "https://x.com/JayFaris6";
+export const EMAIL_URL = "mailto:jayfaris72@gmail.com";
 export const BOOK_LESSON_URL = "https://jayfaris.youcanbook.me";
 export const imgFolder = "images/illustrations/";
 export const animationFolder = "images/animations/";
