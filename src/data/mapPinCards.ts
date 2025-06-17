076a1aaa709a5117bcea9496a8b93c639af115cd
Fiser <ruben@nexolabs.xyz>
2025-06-17 16:40:56 +0200

    feat: small changes

--- src/data/mapPinCards.ts
+++ src/data/mapPinCards.ts
@@ -2,37 +2,30 @@ import type { MapPinCard } from '../types';
 
 export const mapPinCards: MapPinCard[] = [
   {
+    title: "Discovering Hong Kong",
+    description: "¡I was in Hong Kong just before turning back to China!",
+    date: "1997",
+    rotate: -4,
+    delay: 1.2,
+  },
+  {
     title: "Discovering Japan",
-    description: "I lived in Japan for 3 years and I can speak the language fairly well, I am interesting in keep speaking it, I love my Japan friends and their awesome culture",
-    date: "2007 - 2010",
+    description: "I lived in Japan for 10 years and I can speak the language fairly well.",
+    date: "1997-2007",
     rotate: 3,
   },
   {
     title: "Discovering Australia",
-    description: "I lived in Japan for 3 years and I can speak the language fairly well, I am interesting in keep speaking it, I love my Japan friends and their awesome culture",
-    date: "2015 - 2016",
+    description: "I was camping there just in side gold coast for a month.",
+    date: "2006",
     rotate: -6,
     delay: 0.6,
   },
   {
     title: "Discovering Poland",
-    description: "I lived in Japan for 3 years and I can speak the language fairly well, I am interesting in keep speaking it, I love my Japan friends and their awesome culture",
+    description: "I was working in Poland for 2 years and now I know how to say 'hello' in Polish.",
     date: "2022 - 2024",
     rotate: 3,
     delay: 0.9,
-  },
-  {
-    title: "Discovering Australia",
-    description: "I lived in Japan for 3 years and I can speak the language fairly well, I am interesting in keep speaking it, I love my Japan friends and their awesome culture",
-    date: "2015 - 2016",
-    rotate: -4,
-    delay: 1.2,
-  },
-  {
-    title: "Discovering Poland",
-    description: "I lived in Japan for 3 years and I can speak the language fairly well, I am interesting in keep speaking it, I love my Japan friends and their awesome culture",
-    date: "2022 - 2024",
-    rotate: 8,
-    delay: 1.5,
-  },
+  }
 ];
